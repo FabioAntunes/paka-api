4424b3dab0b32253270a37b85028e93af1d0fc63
Fábio Antunes <fabioantuness@gmail.com>
2015-04-12 23:48:56 +0100

    Simplified paka token middleware and updated tokenizer class

--- app/Http/Middleware/VerifyPakaToken.php
+++ app/Http/Middleware/VerifyPakaToken.php
@@ -5,28 +5,25 @@ use Tokenizer;
 
 class VerifyPakaToken {
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-		$apiKey = $request->header('X-API-KEY', false);
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $apiKey = $request->header('X-API-KEY', false);
 
-		if($apiKey) {
-            if (Tokenizer::authenticate($apiKey)) {
-		        return $next($request);
-            }else{
-                return response('Unauthorized.', 401);
-            }
-        } else{
-            return response('Forbidden.', 500);
-
+        if ($apiKey && Tokenizer::authenticate($apiKey))
+        {
+            return $next($request);
+        } else
+        {
+            return response('Unauthorized.', 401);
         }
 
-	}
+    }
 
 }
--- app/Paka/Classes/Tokenizer.php
+++ app/Paka/Classes/Tokenizer.php
@@ -5,17 +5,36 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Tokenizer {
 
+    /**
+     * Authenticated user
+     * @var \App\User
+     */
     protected $user;
 
+
+    /**
+     * Authenticates the api key and sets the respective User
+     *
+     * @param string $apiKey
+     * @return bool
+     */
     public function authenticate($apiKey)
     {
         try{
             $token = Token::where('key', '=', $apiKey)->where('expires', '>',  date("Y-m-d H:i:s"))->firstOrFail();
-
             return $this->user = $token->user;
         }catch(ModelNotFoundException $e){
             return false;
         }
 
+    }
+
+    /**
+     * Returns the authenticated user
+     * @return \App\User
+     */
+    public function getUser()
+    {
+        return $this->user;
     }
 }
